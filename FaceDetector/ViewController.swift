e7da58f401fe83b81ef80a0f07fedb68c77f6900
omq78 <omar.qab@gmail.com>
2019-07-13 17:31:43 +0300

    highlight faces

--- FaceDetector/ViewController.swift
+++ FaceDetector/ViewController.swift
@@ -17,11 +17,44 @@ class ViewController: UIViewController {
         guard let image = UIImage(named: "sample1") else {return}
         let imageViewHeight = view.frame.width * (image.size.height / image.size.width)
         let imageView = UIImageView(image: image)
-        imageView.frame = CGRect(x: 0, y: 40, width: self.view.frame.width, height: imageViewHeight)
+        imageView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: imageViewHeight)
         view.addSubview(imageView)
         
+        let request = VNDetectFaceRectanglesRequest { (req, error) in
+            if let err = error {
+                print("error in face detection", err)
+                return
+            }
+            DispatchQueue.main.async {
+                req.results?.forEach({ (res) in
+                    if let result = res as? VNFaceObservation {
+                        print(result.boundingBox)
+                        let detectView = UIView()
+                        detectView.backgroundColor = UIColor.red
+                        detectView.alpha = 0.5
+                        let imageViewStartingX: CGFloat = self.view.frame.width
+                        let imageViewStartingY: CGFloat = imageView.frame.height
+                        let x = imageViewStartingX * result.boundingBox.origin.x
+                        let h = imageViewStartingY * result.boundingBox.height
+                        let y = imageViewStartingY * (1 -  result.boundingBox.origin.y) - h
+                        let w = imageViewStartingX * result.boundingBox.width
+                        detectView.frame = CGRect(x: x, y: y, width: w, height: h)
+                        self.view.addSubview(detectView)
+                    }
+                })
+            }
+        }
         
-        
+        DispatchQueue.global(qos: .background).async {
+            guard let cgImage = image.cgImage else { return }
+            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            do {
+                try handler.perform([request])
+            } catch let error {
+                print("error in face detection", error)
+                return
+            }
+        }
     }
 
 
